e3db77a12f81b0b031abcb24b5914b483b4759a6
Benny Neugebauer <bn@bennyn.de>
2013-07-05 12:45:29 +0200

    Evertything starts with a Java project. =)

---
+++ src/main/java/de/bennyn/javascript/utilities/App.java
@@ -0,0 +1,13 @@
+package de.bennyn.javascript.utilities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
