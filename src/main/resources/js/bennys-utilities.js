3abf3356a93b5ff8068ed5d104c96d749616d4df
Benny Neugebauer <bn@bennyn.de>
2013-07-08 12:20:20 +0200

    Sorted function names in alphabetical order and added JSDoc.

--- src/main/resources/js/bennys-utilities.js
+++ src/main/resources/js/bennys-utilities.js
@@ -1,32 +1,12 @@
 var BENNYS = {};
+
 BENNYS.Utilities = {
-  test: function() {
-    return 'Hello World';
-  },
-  getURLParameter: function(_name, _url) {
-    if (!_url) {
-      _url = location.search;
-    }
-    var value = decodeURIComponent((RegExp(_name + '=' + '(.+?)(&|$)').exec(_url) || [, ''])[1]);
-    // value is not set but the parameter can still be used as a boolean flag (example.html?activate)
-    if (value.length === 0) {
-      value = (new RegExp('[?|&]' + _name + '(?:[=|&|#|;|]|$)', 'i').exec(_url) !== null);
-    }
-    return value;
-  },
-  getFileExtension: function(filePath) {
-    var extension = '';
-
-    if (typeof filePath === 'string') {
-      extension = filePath.substr((~-filePath.lastIndexOf('.') >>> 0) + 2);
-    }
-
-    return extension;
-  },
-  isHexColorCode: function(string) {
-    var regex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/g;
-    return regex.test(string);
-  },
+  /**
+   * Splits a given string into an object hierarchy.
+   * 
+   * @param {string} name Package name to be created as an object.
+   * @returns {object} Returns an "object" or "undefined" (if the package name is not a string).
+   */
   createNameSpace: function(name) {
     if (typeof name !== 'string')
       return undefined;
@@ -45,7 +25,61 @@ BENNYS.Utilities = {
 
       parent = parent[parts[i]];
     }
-    
+
     return parent;
+  },
+  /**
+   * Returns the extension of a given file or file path.
+   * 
+   * @param {string} filePath File or file path.
+   * @returns {string} File extension (ex. "js" or "css").
+   */
+  getFileExtension: function(filePath) {
+    var extension = '';
+
+    if (typeof filePath === 'string') {
+      extension = filePath.substr((~-filePath.lastIndexOf('.') >>> 0) + 2);
+    }
+
+    return extension;
+  },
+  /**
+   * Returns the value (string) of an URL parameter.
+   * If the URL parameter is given without any value (ex. index.html?param), then it returns true.
+   * If the URL parameter is not given (ex. index.html), then it returns false.
+   * 
+   * @param {type} name Name of the parameter to be searched for.
+   * @param {type} url URL to be searched. If no URL is passed, the current address will be used.
+   * @returns {string} Parameter value
+   */
+  getURLParameter: function(name, url) {
+    if (!url) {
+      url = location.search;
+    }
+
+    var value = decodeURIComponent((RegExp(name + '=' + '(.+?)(&|$)').exec(url) || [, ''])[1]);
+    // value is not set but the parameter can still be used as a boolean flag (example.html?activate)
+    if (value.length === 0) {
+      value = (new RegExp('[?|&]' + name + '(?:[=|&|#|;|]|$)', 'i').exec(url) !== null);
+    }
+
+    return value;
+  },
+  /**
+   * Checks if a color is valid (ex. #00FF00).
+   * 
+   * @param {type} string Color code
+   * @returns {boolean} Information whether it is a valid hex code or not.
+   */
+  isHexColorCode: function(string) {
+    var regex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/g;
+    return regex.test(string);
+  },
+  /**
+   * Returns "Hello World" to test Benny's Utilities.
+   * @returns {string} "Hello World"
+   */
+  test: function() {
+    return 'Hello World';
   }
 };
